8a91769d6428712263ca975c03ccbeb337cf5a84
madunn123 <donquixoteramadhan9@gmail.com>
2023-09-29 17:50:24 +0700

    [fe] - slicing form login & add react-icons

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BiMoon } from 'react-icons/bi';
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <section className="relative p-20">
+      <div className="w-10 h-10 p-2 rounded-full bg-secondary-color" />
+      <div className="p-2 rounded-full w-14 h-14 bg-secondary-color" />
+
+      <div className="p-10 bg-primary-color shadow-for-cards relative rounded-[20px]">
+        lgin
+        <img src="/images/login-cloud.svg" alt="cloud-icons" />
+
+        <div className="absolute xl:top-6 xl:right-6">
+          <BiMoon className="xl:text-4xl text-active-color" />
+        </div>
+      </div>
+    </section>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      'primary-color': '#F8F6F4',
+      'secondary-color': '#E3F4F4',
+      'active-color': '#71A8A8',
+      'thirdy-color': '#87B5B5',
+      'sub-text': '#4B4B4B',
+      'head-text': '#454545',
+      'border-color': '#AEACAB',
+      'placeholder-color': '#606060',
+      'text-color': '#F3F3F3',
+    },
   },
   plugins: [],
 };
