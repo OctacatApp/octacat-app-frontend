c0846228214aa0bed9caebe714f748d3a30d7a28
madunn123 <donquixoteramadhan9@gmail.com>
2024-04-29 20:59:14 +0700

    fix route validation

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes, useNavigate } from 'react-router-dom';
 
 import { useEffect } from 'react';
-// import Login from '@/pages/login';
 import Register from '@/pages/register';
 import SendEmail from '@/pages/auth/sendEmail';
 import ChangePassword from '@/pages/auth/changePassword';
 import { recoverSession } from '@/utils/session';
 import Chat from '@/pages/chat';
+import Login from '@/pages/login';
 
 export default function Routers() {
   const user = recoverSession('token');
@@ -18,23 +18,19 @@ export default function Routers() {
     }
   }, [user]);
 
-  // if (!user) {
-  //   return (
-  //     <Routes>
-  //       <Route path="/" element={<Login />} />
-  //       <Route path="/auth/sign-up" element={<Register />} />
-  //       <Route path="/auth/send-email" element={<SendEmail />} />
-  //       <Route path="/auth/change-password" element={<ChangePassword />} />
-  //     </Routes>
-  //   );
-  // }
+  if (!user) {
+    return (
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/auth/sign-up" element={<Register />} />
+        <Route path="/auth/send-email" element={<SendEmail />} />
+        <Route path="/auth/change-password" element={<ChangePassword />} />
+      </Routes>
+    );
+  }
 
   return (
     <Routes>
-      {/* <Route path="/" element={<Login />} /> */}
-      <Route path="/auth/sign-up" element={<Register />} />
-      <Route path="/auth/send-email" element={<SendEmail />} />
-      <Route path="/auth/change-password" element={<ChangePassword />} />
       <Route path="/chat" element={<Chat />} />
     </Routes>
   );
